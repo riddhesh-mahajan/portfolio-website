6f8a9fb4de4289e5a616855ecebc6b38e9fc31d7
riddhesh-mahajan <riddheshgp@gmail.com>
2021-11-11 20:21:29 +0530

    feat: create projects partial

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,46 @@
 class HomeController < ApplicationController
   def index
+    @projects = [
+      {
+        'name': 'Demo', 
+        'description': 'Description Description Description Description Description',
+        'tags': ['AWS', 'Ruby on rails']
+      },
+      {
+        'name': 'Demo', 
+        'description': 'Description Description Description Description Description',
+        'tags': ['AWS', 'Ruby on rails']
+      },
+      {
+        'name': 'Demo', 
+        'description': 'Description Description Description Description Description',
+        'tags': ['AWS', 'Ruby on rails']
+      },
+      {
+        'name': 'Demo', 
+        'description': 'Description Description Description Description Description',
+        'tags': ['AWS', 'Ruby on rails']
+      },
+      {
+        'name': 'Demo', 
+        'description': 'Description Description Description Description Description',
+        'tags': ['AWS', 'Ruby on rails']
+      },
+      {
+        'name': 'Demo', 
+        'description': 'Description Description Description Description Description',
+        'tags': ['AWS', 'Ruby on rails']
+      },
+      {
+        'name': 'Demo', 
+        'description': 'Description Description Description Description Description',
+        'tags': ['AWS', 'Ruby on rails']
+      },
+      {
+        'name': 'Demo', 
+        'description': 'Description Description Description Description Description',
+        'tags': ['AWS', 'Ruby on rails']
+      }
+    ]
   end
 end
